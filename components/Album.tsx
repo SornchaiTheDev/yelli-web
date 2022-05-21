0b66804f53268b65b4fa49d12267cd443162d0ad
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-05-21 14:47:16 +0700

    Modified : Album Component

--- components/Album.tsx
+++ components/Album.tsx
@@ -1,42 +1,39 @@
-import Link from "next/link";
-import { AlbumProps } from "@decor/Album";
+import { AlbumProps } from '@decor/Album';
 const Album = ({ imgset, name, amount, id }: AlbumProps) => {
   return (
-    <div className="flex flex-col items-center gap-4 col-span-1 ">
+    <div className="flex flex-col items-center gap-4 col-span-1">
       <div className="grid grid-cols-2 gap-2 justify-items-stretch place-items-stretch w-full h-full">
         {imgset
-          .filter((_, index) =>
-            imgset.length > 1 ? index < imgset.length - 1 : true
-          )
-          .map(({ src }, index) => (
-            <Link href={`/gallery/${id}`} key={src}>
-              <div className="rounded-lg overflow-hidden bg-gray-50  aspect-w-3 aspect-h-2 cursor-pointer h-fit">
-                {src !== null && (
-                  <img
-                    className="h-full object-cover object-center pointer-events-none"
-                    src={src!}
-                  />
-                )}
-              </div>
-            </Link>
-          ))}
-        <Link href="/gallery/test">
-          <div className="rounded-lg overflow-hidden bg-gray-50 relative  aspect-w-3 aspect-h-2 cursor-pointer">
-            {imgset.filter(({ src }) => src !== null).length > 3 && (
-              <>
-                <div className="bg-[rgba(0,0,0,.40)] top-0 left-0 w-full h-full absolute flex justify-center items-center z-10">
-                  <h2 className="text-white font-semibold text-xl">
-                    +{amount - 4}
-                  </h2>
-                </div>
+          .filter((_, index) => index < imgset.length - 1)
+          .map(({ src }) => (
+            <div
+              key={src}
+              className="rounded-lg overflow-hidden bg-gray-50  aspect-w-3 aspect-h-2 cursor-pointer h-fit"
+            >
+              {src !== null && (
                 <img
                   className="h-full object-cover object-center pointer-events-none"
-                  src={imgset[imgset.length - 1].src!}
+                  src={src!}
                 />
-              </>
-            )}
-          </div>
-        </Link>
+              )}
+            </div>
+          ))}
+
+        <div className="rounded-lg overflow-hidden bg-gray-50 relative  aspect-w-3 aspect-h-2 cursor-pointer">
+          {amount > 4 && (
+            <div className="bg-[rgba(0,0,0,.40)] top-0 left-0 w-full h-full absolute flex justify-center items-center z-10">
+              <h2 className="text-white font-semibold text-xl">
+                +{amount - 4}
+              </h2>
+            </div>
+          )}
+          {imgset[imgset.length - 1].src !== null && (
+            <img
+              className="h-full object-cover object-center pointer-events-none"
+              src={imgset[imgset.length - 1].src!}
+            />
+          )}
+        </div>
       </div>
       <h2 className="text-center">{name}</h2>
     </div>
